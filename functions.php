ef1d60655697610d8e509db0f2c879e39e0b70cc
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 12:59:54 -0400

    Listagem do banco

--- functions.php
+++ functions.php
@@ -2,6 +2,14 @@
 //$con = mysql_connect('HOSTNAME','USERNAME','PASSWORD');
 
 
+	function limitString($texto, $limit){
+		if(strlen($texto) > $limit){
+			$novoTexto = substr($texto, 0, $limit);
+			return $novoTexto . '...';
+		}else{
+			return $texto;
+		}
 
+	}
 
- ?>
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!-- Esse comando include do php serve para incluir outros arquivos. -->
 <!-- O __DIR__ serve para passar o endereco do arquivo atual. -->
 <?php include(__DIR__.'/header.php'); ?>
+<?php include(__DIR__.'/functions.php'); ?>
 
 <?php 
 
@@ -9,7 +10,7 @@ $ch = curl_init();
 $options = [
     CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_URL            => 'http://localhost:3000/noticias'
+    CURLOPT_URL            => 'https://bloconotas.herokuapp.com/anotacao'
 ];
 
 curl_setopt_array($ch, $options);
@@ -19,18 +20,18 @@ curl_close($ch);
 ?>
 
 <body>
-	<?php /* ?>
+	<?php  ?>
 	<section class="painel-notas">
 		<div class="container">
 			<div class="row">
-				<?php foreach ($noticias as $value) : ?>
+				<?php foreach ($noticias->rows as $value) : ?>
 					<div class="col-12 col-md-4 mb-3">
-						<div class="card mx-auto text-center" data-id="<?php echo $value->id_noticia; ?>">
+						<div class="card mx-auto text-center" data-id="<?php echo $value->id_anotacao; ?>">
 							<img class="card-img-top" src="./img/bloco-notas.jpg" alt="Card image cap">
 							<!-- <i class="fa fa-desktop" aria-hidden="true"></i> -->
 							<div class="card-body">
 								<h5 class="card-title"><?php echo $value->titulo; ?></h5>
-								<p class="card-text"><?php echo $value->noticia; ?></p>
+								<p class="card-text"><?php echo limitString($value->texto, 150); ?></p>
 								<a href="#" class="btn btn-primary">Entrar</a>
 							</div>
 						</div>
@@ -39,7 +40,9 @@ curl_close($ch);
 			</div>
 		</div>
 	</section>
-	<?php */ ?>
+	<?php  ?>
+
+	<?php /* ?>
 	<section class="painel-notas">
 		<div class="container">
 			<div class="row">
@@ -59,6 +62,7 @@ curl_close($ch);
 			</div>
 		</div>
 	</section>
+	<?php */ ?>
 </body>
 
 <?php include(__DIR__.'/footer.php'); ?>
